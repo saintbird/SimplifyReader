71cb930d4392259a429049263523bae6a4361610
jeffer.gu <jeffery.gu@cootek.cn>
2016-03-11 15:00:44 +0800

    remove useless files

--- library/src/main/java/com/github/obsessive/library/swipeback/SwipeBackLayout.java
+++ library/src/main/java/com/github/obsessive/library/swipeback/SwipeBackLayout.java
@@ -21,7 +21,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
